a188f29b547950dbb9514bb589af1f3107776d5d
b4lisong <5397809+b4lisong@users.noreply.github.com>
2024-08-02 10:06:36 -0700

    remove unwanted dock items

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -76,14 +76,7 @@ in
       entries = [
         { path = "/Applications/Slack.app/"; }
         { path = "/System/Applications/Messages.app/"; }
-        { path = "/System/Applications/Facetime.app/"; }
         { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-        { path = "/System/Applications/Music.app/"; }
-        { path = "/System/Applications/News.app/"; }
-        { path = "/System/Applications/Photos.app/"; }
-        { path = "/System/Applications/Photo Booth.app/"; }
-        { path = "/System/Applications/TV.app/"; }
-        { path = "/System/Applications/Home.app/"; }
         {
           path = toString myEmacsLauncher;
           section = "others";
