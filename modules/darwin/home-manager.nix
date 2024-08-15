60b18b9a1eb6d69a451b69f471e3f3e2fadb9243
b4lisong <5397809+b4lisong@users.noreply.github.com>
2024-08-14 17:36:20 -0700

    vimari safari plugin

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -71,8 +71,8 @@ in
     masApps = {
       # "1password" = 1333542190; # using cask instead
       "1Password for Safari" = 1569813296;
+      "Vimari" = 1480933944; 
       "wireguard" = 1451685025;
-      # "tailscale" = 1475387142; # not working? use cask?
     };
   };
 
