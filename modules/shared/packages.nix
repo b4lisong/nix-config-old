827ce8d041c3c2f05276db626e277014fdc95c25
b4lisong <5397809+b4lisong@users.noreply.github.com>
2024-08-17 19:41:37 -0700

    move to smaller shared packages, define more macOS

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,55 +1,23 @@
+## Shared packages across all environments
+## Keep this small!
 { pkgs }:
 
 with pkgs; [
-  # General packages for development and system management
-  aspell
-  aspellDicts.en
+  # Development & system mgmt
   bash-completion
   bat
   coreutils
+  difftastic
   killall
-  lazygit
-  neofetch
-  nnn
-  sqlite
   wget
 
   # System monitoring
   btop
   iftop
-
-  # Common clients for various protocol connections
-  freerdp3
-  ntp
-  openssh
-  samba
-  tigervnc
-
-  # Encryption and security tools
-  age
-  age-plugin-yubikey
-  gnupg
-  libfido2
-
-  # Cloud-related tools and SDKs
-  # move docker to nixos-specific config; use orbstack for macos
-
-  # Media-related packages
-  dejavu_fonts
-  ffmpeg
-  fd
-  font-awesome
-  hack-font
-  noto-fonts
-  noto-fonts-emoji
-  meslo-lgs-nf
-
-  # Text and terminal utilities
   htop
-  hunspell
-  jetbrains-mono
+
+  # Text & terminal utilities
   jq
-  kitty
   ripgrep
   tree
   tmux
@@ -62,67 +30,6 @@ with pkgs; [
   p7zip
   zip
 
-  # Python packages
-  # Seems to be an issue with python 3.12?
-  #python3
-  #virtualenv
-  python39
-  python39Packages.virtualenv # globally install virtualenv
-  #python311 # also failing
-
-  ## Pentest tools
-  # Network
-  netdiscover
-  ncftp
-  netcat-gnu
-  responder
-  whois
-  # Password / Hashing
-  hashcat
-  hashcat-utils
-  john
-  thc-hydra
-  # Port scan
-  nmap
-  rustscan
-  # Proxy & pivoting
-  chisel
-  ligolo-ng
-  proxychains-ng
-  # Fuzzing
-  feroxbuster
-  ffuf
-  gobuster
-  # RE / Malware Analysis
-  radare2
-  binwalk
-  # Services
-  #metasploit
-  sqlmap
-  braa
-  onesixtyone
-  snmpcheck
-  davtest
-  # Web
-  apachetomcatscanner
-  nikto
-  whatweb
-  wprecon
-  #wpscan
-  # Windows
-  bloodhound-py
-  enum4linux-ng
-  gomapenum
-  kerbrute
-  nbtscan
-  nbtscanner
-  samba
-  smbmap
-  smbscan
-  # Wireless
-  aircrack-ng
-  # Wordlists
-  rockyou
-  seclists
-
+  # Misc.
+  neofetch
 ]
