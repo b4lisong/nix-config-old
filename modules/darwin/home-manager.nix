2258d0e3311c3ac98caa3b4249b63bb603f4e7ee
b4lisong <5397809+b4lisong@users.noreply.github.com>
2024-08-26 10:07:05 -0700

    fix

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -91,6 +91,45 @@ in
         stateVersion = "23.11";
       };
       programs = {
+        kitty = {
+          enable = true;
+          darwinLaunchOptions = [ "--single-instance" ];
+          shellIntegration.enableZshIntegration = true;
+          font = {
+            name = "MesloLGS NF";
+            size = 11;
+          };
+          theme = "Catppuccin-Mocha";
+          keybindings = {
+            "f1" = "show_kitty_env_vars";
+          };
+          settings = {
+            background_opacity = "0.9";
+            background_tint = "0.9";
+            background_blur = 10;
+            dim_opacity = "1.0";
+            tab_bar_edge = "bottom";
+            tab_bar_margin_width = "20.0";
+            tab_bar_margin_height = "20.0 0.0";
+            tab_bar_style = "powerline";
+            tab_bar_align = "left";
+            tab_bar_min_tabs = 2;
+            tab_switch_strategy = "previous";
+            tab_activity_symbol = "none";
+            tab_powerline_style = "slanted";
+            tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}";
+            active_tab_title_template = "none";
+            scrollback_lines = 10000;
+            enable_audio_bell = false;
+            window_margin_width = 0;
+            window_padding_width = 20;
+            remember_window_size = true;
+            placement_strategy = "center";
+            confirm_os_window_close = 0;
+            hide_window_decorations = "titlebar-only";
+            macos_quit_when_last_window_closed = true;
+          };
+        };   
       } // import ../shared/home-manager.nix { inherit config pkgs lib; };
 
       # Marked broken Oct 20, 2022 check later to remove this
@@ -99,45 +138,6 @@ in
     };
   };
 
-  kitty = {
-    enable = true;
-    darwinLaunchOptions = [ "--single-instance" ];
-    shellIntegration.enableZshIntegration = true;
-    font = {
-      name = "MesloLGS NF";
-      size = 11;
-    };
-    theme = "Catppuccin-Mocha";
-    keybindings = {
-      "f1" = "show_kitty_env_vars";
-    };
-    settings = {
-      background_opacity = "0.9";
-      background_tint = "0.9";
-      background_blur = 10;
-      dim_opacity = "1.0";
-      tab_bar_edge = "bottom";
-      tab_bar_margin_width = "20.0";
-      tab_bar_margin_height = "20.0 0.0";
-      tab_bar_style = "powerline";
-      tab_bar_align = "left";
-      tab_bar_min_tabs = 2;
-      tab_switch_strategy = "previous";
-      tab_activity_symbol = "none";
-      tab_powerline_style = "slanted";
-      tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}";
-      active_tab_title_template = "none";
-      scrollback_lines = 10000;
-      enable_audio_bell = false;
-      window_margin_width = 0;
-      window_padding_width = 20;
-      remember_window_size = true;
-      placement_strategy = "center";
-      confirm_os_window_close = 0;
-      hide_window_decorations = "titlebar-only";
-      macos_quit_when_last_window_closed = true;
-    };
-  };   
 
   # System services
   services = {
