f0ad2a5ccfaeba5fab64a3be7b48a9dcdb9d89ff
JL <5397809+b4lisong@users.noreply.github.com>
2024-11-05 15:44:34 -0800

    enable audio/bluetooth/peripherals

---
+++ home/linux/gui/base/media.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}:
+# media - control and enjoy audio/video
+{
+  home.packages = with pkgs; [
+    # audio control
+    pavucontrol
+    playerctl
+    pulsemixer
+    imv # simple image viewer
+  ];
+
+  programs = {
+    mpv = {
+      enable = true;
+      defaultProfiles = ["gpu-hq"];
+      scripts = [pkgs.mpvScripts.mpris];
+    };
+  };
+
+  services = {
+    playerctld.enable = true;
+  };
+}
